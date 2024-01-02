f076c233f7998860421fd1f52be8cb31d238f37e
HARINI <harini@HARINIs-MacBook-Pro.local>
2024-01-02 21:05:21 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,196 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Find Your Perfect Partner - Matrimony | About :: Matrimony
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+ <?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1">
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page">About</li>
+     </ul>
+   </div>
+   <div class="about">
+   	  <div class="col-md-6 about_left">
+   	  	<img src="images/a3.jpg" class="img-responsive" alt=""/>
+   	  </div>
+   	  <div class="col-md-6 about_right">
+   	  	<h1>About us</h1>
+   	  	<p>"Welcome to HappilyEverAfter.com - the leading online platform for finding your soulmate and life partner. Our mission is to help individuals find true love and lifelong companionship through our innovative matchmaking tools and personalized approach.
+
+At HappilyEverAfter.com, we believe that everyone deserves to find happiness in love and marriage. That's why we've designed our platform to be user-friendly, secure, and effective. Our team of experts is committed to providing the highest level of customer support and guidance to help you navigate the journey of finding your perfect match.
+
+Founded in 2010, HappilyEverAfter.com has helped thousands of singles find their partners and build successful marriages. Our unique algorithm and comprehensive profiles make it easy to connect with like-minded individuals who share your values and interests.
+
+Don't just take our word for it - hear what some of our success stories have to say about their experience with HappilyEverAfter.com:
+
+'After years of unsuccessful dating, I was skeptical about online matchmaking. But HappilyEverAfter.com proved me wrong. Within a few weeks of signing up, I found my soulmate and we've been happily married for two years now!' - Sarah, 32.
+
+Ready to start your journey to finding love and happiness? Sign up for free today and let us help you find your HappilyEverAfter."</p>
+
+		</div>
+   	  </div>
+   	  <div class="clearfix"> </div>
+   </div>
+  </div>
+</div>
+<div class="about_middle">
+	<div class="container">
+	  <h2>Happy Clients</h2>
+	  <div class="about_middle-grid1">
+		<div class="col-sm-6 testi_grid list-item-0">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a1.jpg" class="img-responsive" alt=""/>
+				</figure>	
+				<div><a href="#">Best website ever seen , living a beautiful life</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+		    <small class="testi-meta"><span class="user">Swetha</span></small>
+		</div>
+		<div class="col-sm-6 testi_grid list-item-1">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a2.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">My life is settled!!!</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+			<small class="testi-meta1"><span class="user">Akshay</span></small>
+		</div>
+		<div class="clearfix"> </div>
+	  </div>
+	  <div class="about_middle-grid2">
+		<div class="col-sm-6 testi_grid list-item-0">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a1.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">I am very much hapy with their service , keep developing!!!</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+		    <small class="testi-meta"><span class="user">maddy</span></small>
+		</div>
+		<div class="col-sm-6 testi_grid list-item-1">
+			<blockquote class="testi_grid_blockquote">
+				<figure class="featured-thumbnail">
+					<img src="images/a2.jpg" class="img-responsive" alt=""/>
+				</figure>
+				<div><a href="#">Helphed me to find my best partner!!!!!!</a>
+				<div class="clearfix"></div>
+				</div>
+			</blockquote>
+			<small class="testi-meta1"><span class="user">Rahul</span></small>
+		</div>
+		<div class="clearfix"> </div>
+	  </div>
+	</div>
+</div>
+<div class="about_bottom">
+	<div class="container">
+		<h3>Team</h3>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/a4.jpg"  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Sai Vignesh</a></h4>
+			    	<p>Frontend and backend</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/a5.jpg"  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Ashere Jeswin</a></h4>
+			    	<p>Frontend and Backend</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+	   <div class="col-md-3 about_grid1">
+		  <ul class="posts-grid our-team">
+			<li class="list-item-1">
+				<figure class="thumbnail_1 thumbnail">
+					<a href="#"><img src="images/a6.jpg"  class="img-responsive" alt=""/></a>
+					<div class="post_networks">
+						<ul>
+							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div>
+			    </figure>
+			    <div class="desc">
+			    	<h4><a href="#">Harini K R</a></h4>
+			    	<p>Frontend and backend</p>
+			    </div>
+			 </li>
+	       </ul>
+	   </div>
+
+	   <div class="clearfix"> </div>
+	</div>
+</div>
+
+
+<?php include_once("footer.php");?>
+
+	
---
+++ faq.php
@@ -0,0 +1,118 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Find Your Perfect Partner - Matrimony | Faq :: Matrimony
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+<?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1">
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page">FAQ</li>
+     </ul>
+   </div>
+   <dl class="faq-list">
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How to find a perfect partner???</dt>
+	<dd>
+	<h4 class="marker1">A.</h4>
+	<p class="m_4">Just set the partners preference in the website and go ahead!!!</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>What to do if website crashes??</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">Just wait until we resolve it.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How do I create an account on the website?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">To create an account, simply click the "Sign Up" button and fill in the required information, such as your name, email address, and password. Once you've completed the registration process, you'll be able to log in and access all the features of the website.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How can I search for potential partners on the website?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">Our website has a search function that allows you to filter potential partners by criteria such as age, location, religion, and other important factors. You can also browse profiles and view photos to help you find compatible partners.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>Is the website safe and secure?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">Yes, we take the safety and security of our users very seriously. We use encryption technology to protect your personal information and monitor the website to ensure that all members are genuine and trustworthy.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>How can I improve my chances of finding a compatible partner on the website?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4">There are several things you can do to increase your chances of finding a compatible partner, such as filling out your profile completely, uploading a clear and recent photo, and being proactive in reaching out to other members who share your interests and values. You may also want to consider upgrading to a premium membership to access additional features and benefits.</p>
+	</dd>
+	<dt class="faq-list_h">
+	<h4 class="marker">Q?</h4>
+	<h4>Can I see who has viewed my profile on the website?</h4>
+	</dt>
+	<dd>
+	<h4 class="marker">A.</h4>
+	<p class="m_4 m_5">No, we do not currently offer a feature that allows you to see who has viewed your profile. We believe that privacy is important and want to ensure that all members feel comfortable browsing profiles without fear of being tracked or monitored. However, you can see who has liked or favorited your profile, which can give you an indication of who is interested in getting to know you better.
+
+
+
+</p>
+	</dd>
+   </dl>
+  </div>
+</div>
+
+
+<?php include_once("footer.php");?>
+
